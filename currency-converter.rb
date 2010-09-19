99b4314d67a036fdd8fb820dc13de7fd06fe8501
Hugo Peixoto <hugo.peixoto@gmail.com>
2010-09-19 04:13:49 +0100

    html5 validation. 'Cause I couldn't stop myself

--- currency-converter.rb
+++ currency-converter.rb
@@ -9,16 +9,20 @@ load 'models/currency.rb'
 
 helpers Sinatra::Partials
 
+# Misc gem configuration
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig['production'])
+set :haml, { :format => :html5 }
 
+# Sinatra configuration
 set :views, File.dirname(__FILE__) + "/views"
 set :public, File.dirname(__FILE__) + '/public'
 set :run, true
 enable :static
 
+# Requests
 get '/' do
+  content_type 'text/html', :charset => 'utf-8'
   haml :index
 end
-
 
