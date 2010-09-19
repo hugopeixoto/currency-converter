8582bd5843d29c235639bba043123ce1fbe70e55
Hugo Peixoto <hugo.peixoto@gmail.com>
2010-09-19 01:35:09 +0100

    removed rails stuff

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @currencies = Currency.all
-  end
-end
--- app/models/currency.rb
+++
@@ -1,4 +0,0 @@
-class Currency < ActiveRecord::Base
-  scope :popular, :conditions => { :popular => true }
-  scope :unpopular, :conditions => { :popular => false }
-end
