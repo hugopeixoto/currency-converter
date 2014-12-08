5cfc2c4e2bc2d27b2e8c82193abb0ebf64ee8368
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-12-08 18:17:26 +0000

    Fixes sinatra settings

--- currency-converter.rb
+++ currency-converter.rb
@@ -8,7 +8,6 @@ require 'active_record'
 # Misc gem configuration
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig['production'])
-set :haml, { :format => :html5 }
 
 load 'partials.rb'
 load 'models/currency.rb'
@@ -16,14 +15,13 @@ load 'models/currency.rb'
 helpers Sinatra::Partials
 
 # Sinatra configuration
+set :haml, format: :html5
 set :views, File.dirname(__FILE__) + "/views"
 set :public_dir, File.dirname(__FILE__) + '/public'
-set :run, true
 enable :static
 
 # Requests
 get '/' do
-  content_type 'text/html', :charset => 'utf-8'
+  content_type 'text/html', charset: 'utf-8'
   haml :index
 end
-
